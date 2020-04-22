1434dda48fb164b4fa5b73e36eaa86e9239bf0bd
Tijan Jackson <57716770+TijanJackson@users.noreply.github.com>
2020-04-22 13:43:45 +0200

    weather project commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'isomorphic-fetch'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            main: '',
+            mainMin: '',
+            name: 'City name',
+            celsius: true,
+            fahrenheit: false,
+            kelvin: 273,
+            multiply: 1,
+            thirtyTwo: 0
+        };
+        this.searchWeather = this.searchWeather.bind(this);
+        this.changeToF = this.changeToF.bind(this);
+        this.changeToC = this.changeToC.bind(this);
+    }
+
+    // converts to fahrenheit
+    changeToF(e) {
+        this.setState({
+            celsius: false,
+            fahrenheit: true,
+            multiply: 1.8,
+            thirtyTwo: 32
+
+        })
+    }
+
+    // converts to celsius
+    changeToC(e) {
+        this.setState({
+            celsius: true,
+            fahrenheit: false,
+            multiply: 1,
+            thirtyTwo: 0
+
+        })
+    }
+
+   // http://api.openweathermap.org/data/2.5/weather?q=london&APPID=94033fe10a92947fef19fe1f5c0506c7
+
+    searchWeather() {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('weather').value}&APPID=94033fe10a92947fef19fe1f5c0506c7`)
+            .then(res => res.json()) 
+            .then(
+                (result) => {
+                    this.setState({
+                        main: result.main.temp_max,
+                        mainMin: result.main.temp_min,
+                        name: result.name
+                    });
+                },
+                // error handling takes place here
+                (error) => {
+                    this.setState({
+                        error
+                    });
+                })
+    }
+
+    render() {
+     const { error,  main, mainMin, multiply, thirtyTwo, name} = this.state;
+     if (error) {
+         return <div>Error: {error.message}</div>;
+     } 
+    else {
+         return (
+             <div className="App">
+               <h1 id="title">Weather App</h1>
+               <form>
+                   <div id="type">
+                   <label>Type a City Name: </label>
+                   <input id="weather" type="text" placeholder=" Type a city name here"></input>
+                   </div>
+               </form>
+               <div id="buttons">
+               <button className="btn" id="search" onClick={this.searchWeather}>Check temperature</button>
+               <br></br>
+               <br></br>
+               <button className="btn" id="changeF" onClick={this.changeToF}>Change to fahrenheit</button>
+               <button className="btn" id="changeC" onClick={this.changeToC}>Change to celsius</button>
+               </div>
+               {/* Displays the initial value, then after the search function has run, it displays
+               the value retrieved and subtracts 273.15 from it in order to display the weather in celsius */}
+               <h2 id="weatherName">{name}</h2>
+               <div id="weatherDetails">
+               <h3>Max temp: {Math.round( ((main - 273.15) * multiply) + thirtyTwo)}°C</h3>
+               <h3>Min Temp: {Math.round( ((mainMin - 273.15) * multiply) + thirtyTwo)}°C</h3>
+               </div>
+            </div>
+         );
+     }
+ }
+ }
+
+
+ export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,102 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+// test('the data is peanut butter', async () => {
+//   const data = await fetchData();
+//   expect(data).toBe('peanut butter');
+// });
+
+// test('the fetch fails with an error', async () => {
+//   expect.assertions(1);
+//   try {
+//     await fetchData();
+//   } catch (e) {
+//     expect(e).toMatch('error');
+//   }
+// });
+
+
+
+// test('the data is peanut butter', () => {
+//   return fetchData().then(data => {
+//     expect(data).toBe('peanut butter');
+//   });
+// });
+
+// test('renders correctly', () => {
+//   const tree = renderer
+//   .create(<App></App>)
+//   .toJSON();
+//   expect(tree).toMatchSnapshot();
+//   });
+  
+
+// test('renders correctly', () => {
+//   const tree = renderer
+//   .create(<App></App>)
+//   .toJSON();
+//   expect(tree).toMatchSnapshot();
+//   });
+  
+
+// test('the data should be the weather', () => {
+//   return fetch().then(data => {
+//     expect(data).toBe('weather');
+//   });
+// });
+
+// test('the data is weather', () => {
+//   return expect(fetchData()).resolves.toBe('weather');
+// });
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// function searchWeather() {
+//   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('weather').value}&APPID=94033fe10a92947fef19fe1f5c0506c7`)
+//       .then(res => res.json()) 
+//       .then(
+//           (result) => {
+//               this.setState({
+//                   main: result.main
+//               });
+//           },
+//           // error handling takes place here
+//           (error) => {
+//               this.setState({
+//                   error
+//               });
+//           })
+// }
+
+//   fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=94033fe10a92947fef19fe1f5c0506c7`)
+//       .then(res => res.json()) 
+//       .then(
+//           (result) => {
+//               this.setState({
+//                   main: result.main
+//               });
+//           },
+//           // error handling takes place here
+//           (error) => {
+//               this.setState({
+//                   error
+//               });
+//           })
+
+
+test('fetches data', () => {
+  expect(fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=94033fe10a92947fef19fe1f5c0506c7`)).toBe({"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":286.97,"feels_like":281.5,"temp_min":284.82,"temp_max":288.71,"pressure":1004,"humidity":82},"visibility":10000,"wind":{"speed":8.2,"deg":240},"clouds":{"all":90},"dt":1583845089,"sys":{"type":1,"id":1414,"country":"GB","sunrise":1583821515,"sunset":1583862983},"timezone":0,"id":2643743,"name":"London","cod":200});
 });
+
